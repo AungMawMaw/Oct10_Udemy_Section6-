e9a61e2a5b0ee4af31b4b170051b1f4aa60d053a
AungMawMaw <aungmaw.bc@gmail.com>
2024-10-11 09:56:48 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    DashboardItemComponent,
+    TicketsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Oct10_Udemy_Section6';
+
+  currentStatus = 'online';
 }
---
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+type Status = 'online' | 'offline' | 'unknown';
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.css',
+})
+export class ServerStatusComponent {
+  currentStatus: Status = 'online';
+
+  constructor() {}
+  ngOnInit() {
+    setInterval(() => {
+      const random = Math.random();
+
+      if (random < 0.5) {
+        this.currentStatus = 'online';
+      } else if (random < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+}
